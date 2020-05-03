7886788f724a797b9d7ca8d6193a0803be6249f0
Muzafar Ali <muzfr7@gmail.com>
2020-05-03 12:55:56 +0800

    added webgrind service in docker-compose.yml

--- app/index.php
+++ app/index.php
@@ -1,5 +1,7 @@
 <?php
 
+include("math.php");
+
 $num1 = 1;
 $num2 = 2;
 
@@ -8,3 +10,7 @@ $sum = $num1 + $num2;
 echo $sum;
 
 echo "Hello World..";
+
+
+
+echo sum($num1, $num2);
---
+++ app/math.php
@@ -0,0 +1,10 @@
+<?php
+
+function sum($num1, $num2){
+	helper();
+	return $num1 + $num2;
+}
+
+function helper() {
+	echo "I'm helper..";
+}
